e09e93638df880a3e8865b908614384c1e5aa77a
Mostafa Anter <ga30000@gmail.com>
2016-05-24 16:47:58 +0200

    stable version

---
+++ app/src/main/java/perfect_apps/sharkny/activities/MyAccountActivity.java
@@ -0,0 +1,102 @@
+package perfect_apps.sharkny.activities;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.akexorcist.localizationactivity.LocalizationActivity;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import perfect_apps.sharkny.R;
+
+public class MyAccountActivity extends LocalizationActivity {
+
+    @Bind(R.id.text1) TextView textView1;
+    @Bind(R.id.text2) TextView textView2;
+    @Bind(R.id.text3) TextView textView3;
+    @Bind(R.id.text4) TextView textView4;
+    @Bind(R.id.text5) TextView textView5;
+
+    @Bind(R.id.linear1) LinearLayout linearLayout1;
+    @Bind(R.id.linear2) LinearLayout linearLayout2;
+    @Bind(R.id.linear3) LinearLayout linearLayout3;
+    @Bind(R.id.linear4) LinearLayout linearLayout4;
+    @Bind(R.id.linear5) LinearLayout linearLayout5;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_account);
+        ButterKnife.bind(this);
+        setToolbar();
+        changeTextFont();
+
+    }
+
+    private void setToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_navigate_before_black_24dp);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MyAccountActivity.this, HomeActivity.class)
+                        .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK));
+                overridePendingTransition(R.anim.push_left_enter, R.anim.push_left_exit);
+                finish();
+            }
+        });
+
+        /*
+        * hide title
+        * */
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        //toolbar.setNavigationIcon(R.drawable.ic_toolbar);
+        toolbar.setTitle("");
+        toolbar.setSubtitle("");
+        //toolbar.setLogo(R.drawable.ic_toolbar);
+
+        /*
+        * change font of title
+        * */
+        TextView mTitle = (TextView) toolbar.findViewById(R.id.toolbar_title);
+        Typeface sharknyFont;
+        if (getLanguage().equalsIgnoreCase("en")){
+            sharknyFont = Typeface.createFromAsset(getAssets(), "fonts/daisy2.ttf");
+        }else {
+            sharknyFont = Typeface.createFromAsset(getAssets(), "fonts/daisy.ttf");
+        }
+        mTitle.setTypeface(sharknyFont);
+
+
+    }
+
+    // set on back button pressed
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        startActivity(new Intent(MyAccountActivity.this, HomeActivity.class)
+                .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK));
+        overridePendingTransition(R.anim.push_left_enter, R.anim.push_left_exit);
+        finish();
+    }
+
+    private void changeTextFont(){
+        Typeface sharknyFont = Typeface.createFromAsset(getAssets(), "fonts/thin.ttf");
+
+        textView1.setTypeface(sharknyFont);
+        textView2.setTypeface(sharknyFont);
+        textView3.setTypeface(sharknyFont);
+        textView4.setTypeface(sharknyFont);
+        textView5.setTypeface(sharknyFont);
+    }
+
+}
--- app/src/main/java/perfect_apps/sharkny/fragments/FragmentOne.java
+++ app/src/main/java/perfect_apps/sharkny/fragments/FragmentOne.java
@@ -1,18 +1,32 @@
 package perfect_apps.sharkny.fragments;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import perfect_apps.sharkny.R;
+import perfect_apps.sharkny.activities.HomeActivity;
+import perfect_apps.sharkny.activities.MyAccountActivity;
 
 /**
  * Created by mostafa on 23/05/16.
  */
 public class FragmentOne extends Fragment {
+    @Bind(R.id.button1) Button button1;
+    @Bind(R.id.button2) Button button2;
+    @Bind(R.id.button3) Button button3;
+    @Bind(R.id.button4) Button button4;
+    @Bind(R.id.button5) Button button5;
+    @Bind(R.id.button6) Button button6;
+
     public FragmentOne(){
 
     }
@@ -21,7 +35,65 @@ public class FragmentOne extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_one, container, false);
+        ButterKnife.bind(this, view);
 
+        changeTypeFace();
+        setButtonsClick();
         return view;
+    }
+
+    private void changeTypeFace(){
+        Typeface sharknyFont = Typeface.createFromAsset(getActivity().getAssets(), "fonts/thin.ttf");
+        button1.setTypeface(sharknyFont);
+        button2.setTypeface(sharknyFont);
+        button3.setTypeface(sharknyFont);
+        button4.setTypeface(sharknyFont);
+        button5.setTypeface(sharknyFont);
+        button6.setTypeface(sharknyFont);
+    }
+
+    private void setButtonsClick(){
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((HomeActivity) getActivity()).bottomNavigationView.selectTab(1);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((HomeActivity) getActivity()).bottomNavigationView.selectTab(1);
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((HomeActivity) getActivity()).bottomNavigationView.selectTab(2);
+            }
+        });
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((HomeActivity) getActivity()).bottomNavigationView.selectTab(1);
+            }
+        });
+
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((HomeActivity) getActivity()).bottomNavigationView.selectTab(3);
+            }
+        });
+
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), MyAccountActivity.class));
+                getActivity().overridePendingTransition(R.anim.push_left_enter, R.anim.push_left_exit);
+            }
+        });
     }
 }
