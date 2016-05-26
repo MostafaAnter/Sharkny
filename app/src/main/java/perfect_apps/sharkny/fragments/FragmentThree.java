db4981b5bb9d84a2c2a4a4ad02923e105ea90fcc
Mostafa Anter <ga30000@gmail.com>
2016-05-26 23:40:48 +0200

    stable version

--- app/src/main/java/perfect_apps/sharkny/fragments/FragmentThree.java
+++ app/src/main/java/perfect_apps/sharkny/fragments/FragmentThree.java
@@ -1,18 +1,26 @@
 package perfect_apps.sharkny.fragments;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioButton;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import perfect_apps.sharkny.R;
 
 /**
  * Created by mostafa on 23/05/16.
  */
 public class FragmentThree extends Fragment {
+
+    @Bind(R.id.button21) RadioButton radioButton1;
+    @Bind(R.id.button22) RadioButton radioButton2;
+
     public FragmentThree(){
 
     }
@@ -21,7 +29,17 @@ public class FragmentThree extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_three, container, false);
+        ButterKnife.bind(this, view);
+
+        changeFont();
 
         return view;
+    }
+
+    private void changeFont(){
+        Typeface font = Typeface.createFromAsset(getActivity().getAssets(), "fonts/thin.ttf");
+
+        radioButton1.setTypeface(font);
+        radioButton2.setTypeface(font);
     }
 }
--- app/src/main/java/perfect_apps/sharkny/fragments/FragmentTwo.java
+++ app/src/main/java/perfect_apps/sharkny/fragments/FragmentTwo.java
@@ -1,18 +1,28 @@
 package perfect_apps.sharkny.fragments;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioButton;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import perfect_apps.sharkny.R;
 
 /**
  * Created by mostafa on 23/05/16.
  */
 public class FragmentTwo extends Fragment {
+
+    @Bind(R.id.button21) RadioButton radioButton1;
+    @Bind(R.id.button22) RadioButton radioButton2;
+    @Bind(R.id.button23) RadioButton radioButton3;
+    @Bind(R.id.button24) RadioButton radioButton4;
+
     public FragmentTwo(){
 
     }
@@ -21,7 +31,19 @@ public class FragmentTwo extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_two, container, false);
+        ButterKnife.bind(this, view);
+
+        
 
         return view;
+    }
+
+    private void changeFont(){
+        Typeface font = Typeface.createFromAsset(getActivity().getAssets(), "fonts/thin.ttf");
+
+        radioButton1.setTypeface(font);
+        radioButton2.setTypeface(font);
+        radioButton3.setTypeface(font);
+        radioButton4.setTypeface(font);
     }
 }
