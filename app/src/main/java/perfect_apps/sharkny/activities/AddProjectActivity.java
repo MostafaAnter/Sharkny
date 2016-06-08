9825c74aa6170b64a620b756218c3c92e5c5ac64
Mostafa Anter <ga30000@gmail.com>
2016-06-09 00:55:45 +0200

    stable version

--- app/src/main/java/perfect_apps/sharkny/activities/AddProjectActivity.java
+++ app/src/main/java/perfect_apps/sharkny/activities/AddProjectActivity.java
@@ -1,6 +1,7 @@
 package perfect_apps.sharkny.activities;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.net.Uri;
@@ -9,11 +10,13 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
@@ -22,29 +25,42 @@ import android.widget.Toast;
 
 import com.akexorcist.localizationactivity.LocalizationActivity;
 import com.android.volley.Cache;
+import com.android.volley.NetworkResponse;
+import com.android.volley.NoConnectionError;
 import com.android.volley.Request;
 import com.android.volley.Response;
+import com.android.volley.TimeoutError;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.bruce.pickerview.popwindow.DatePickerPopWin;
 import com.bumptech.glide.Glide;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.File;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import cn.pedant.SweetAlert.SweetAlertDialog;
 import me.iwf.photopicker.PhotoPickerActivity;
 import me.iwf.photopicker.utils.PhotoPickerIntent;
+import perfect_apps.sharkny.BuildConfig;
 import perfect_apps.sharkny.R;
 import perfect_apps.sharkny.app.AppController;
 import perfect_apps.sharkny.models.Countries;
 import perfect_apps.sharkny.parse.JsonParser;
+import perfect_apps.sharkny.store.SharknyPrefStore;
+import perfect_apps.sharkny.utils.AppHelper;
+import perfect_apps.sharkny.utils.Constants;
 import perfect_apps.sharkny.utils.Utils;
+import perfect_apps.sharkny.utils.VolleyMultipartRequest;
 
 public class AddProjectActivity extends LocalizationActivity {
 
@@ -59,12 +75,22 @@ public class AddProjectActivity extends LocalizationActivity {
     @Bind(R.id.spinner4) Spinner spinner4;
     @Bind(R.id.spinner1) Button spinner1;
     @Bind(R.id.spinner2) Button spinner2;
+    @Bind(R.id.spinner5) Spinner spinner5;
+
+    // editText
+    @Bind(R.id.editText1) EditText title;
+    @Bind(R.id.editText2) EditText describtion;
+    @Bind(R.id.editText3) EditText grantue;
+    @Bind(R.id.editText4) EditText investValue;
+    @Bind(R.id.editText5) EditText investPrec;
 
 
     private static String type;
     private static String field;
     private static String startDate;
+    private static String country;
     private static String endDate;
+    private static Uri profileImagePath;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -75,6 +101,7 @@ public class AddProjectActivity extends LocalizationActivity {
         setOnLinearSelected();
         fetchFranchisField();
         fetchprojectsType();
+        fetchcountry();
 
     }
 
@@ -148,6 +175,7 @@ public class AddProjectActivity extends LocalizationActivity {
                 ArrayList<String> photos =
                         data.getStringArrayListExtra(PhotoPickerActivity.KEY_SELECTED_PHOTOS);
                 Uri uri = Uri.fromFile(new File(photos.get(0)));
+                profileImagePath = uri;
                 setSelectedPhotoInsideCircleShap(uri);
             }
         }
@@ -210,10 +238,7 @@ public class AddProjectActivity extends LocalizationActivity {
                 // First item is disable and it is used for hint
                 if(position > 0){
                     // Notify the selected item text
-                    type = "" + position;
-                    Toast.makeText
-                            (getApplicationContext(),  selectedItemText + " Done!", Toast.LENGTH_SHORT)
-                            .show();
+                    type = selectedItemText;
                 }
             }
 
@@ -273,10 +298,67 @@ public class AddProjectActivity extends LocalizationActivity {
                 // First item is disable and it is used for hint
                 if(position > 0){
                     // Notify the selected item text
-                    field = "" + position;
-                    Toast.makeText
-                            (getApplicationContext(),  selectedItemText + " Done!", Toast.LENGTH_SHORT)
-                            .show();
+                    field = selectedItemText;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+    }
+
+    private void populateSpinner5(List<String> mlist){
+
+        // you will just change R.array.search & spinner1 reference :)
+
+        //final List<String> plantsList = Arrays.asList(getResources().getStringArray(R.array.national));
+        // Initializing an ArrayAdapter
+        final ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(
+                this,R.layout.spinner_item, mlist){
+            @Override
+            public boolean isEnabled(int position){
+                if(position == 0)
+                {
+                    // Disable the first item from Spinner
+                    // First item will be use for hint
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            @Override
+            public View getDropDownView(int position, View convertView,
+                                        ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+                TextView tv = (TextView) view;
+                if(position == 0){
+                    // Set the hint text color gray
+                    tv.setTextColor(Color.GRAY);
+                }
+                else {
+                    tv.setTextColor(Color.BLACK);
+                }
+                return view;
+            }
+        };
+        spinnerArrayAdapter.setDropDownViewResource(R.layout.spinner_item);
+        spinner5.setAdapter(spinnerArrayAdapter);
+
+        spinner5.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItemText = (String) parent.getItemAtPosition(position);
+                // If user change the default selection
+                // First item is disable and it is used for hint
+                if(position > 0){
+                    // Notify the selected item text
+                    country = selectedItemText;
                 }
             }
 
@@ -394,6 +476,61 @@ public class AddProjectActivity extends LocalizationActivity {
 
 
     }
+    private void fetchcountry() {
+        String url;
+        if (getLanguage().equalsIgnoreCase("en")) {
+            url = "http://sharkny.net/en/api/countries/";
+        } else {
+            url = "http://sharkny.net/en/api/countries/index";
+        }
+
+
+        Cache cache = AppController.getInstance().getRequestQueue().getCache();
+        Cache.Entry entry = cache.get(url);
+        if (entry != null) {
+            try {
+                String data = new String(entry.data, "UTF-8");
+                // handle data, like converting it to xml, json, bitmap etc.,
+                List<String> nationalityList = new ArrayList<>();
+                for (Countries countriy : JsonParser.parseCountriesFeed(data)) {
+                    nationalityList.add(countriy.getTitle());
+                }
+                populateSpinner5(nationalityList);
+
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        } else {
+            // Cached response doesn't exists. Make network call here
+            if (Utils.isOnline(AddProjectActivity.this)) {
+                String tag_string_req = "string_req";
+
+                StringRequest strReq = new StringRequest(Request.Method.GET,
+                        url, new Response.Listener<String>() {
+
+                    @Override
+                    public void onResponse(String response) {
+                        List<String> nationalityList = new ArrayList<>();
+                        for (Countries countriy : JsonParser.parseCountriesFeed(response)) {
+                            nationalityList.add(countriy.getTitle());
+                        }
+                        populateSpinner5(nationalityList);
+
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                    }
+                });
+                // Adding request to request queue
+                AppController.getInstance().addToRequestQueue(strReq, tag_string_req);
+            } else {
+            }
+        }
+
+
+    }
 
     private void fetchprojectsType(){
 
@@ -452,5 +589,198 @@ public class AddProjectActivity extends LocalizationActivity {
         }
 
 
+    }
+
+    private boolean attempAdd(){
+        if (!title.getText().toString().trim().isEmpty()
+                && !describtion.getText().toString().trim().isEmpty()
+                && !startDate.trim().isEmpty()
+                && !type.trim().isEmpty()
+                && !field.trim().isEmpty()
+                && !investValue.getText().toString().trim().isEmpty()
+                && !country.trim().isEmpty()
+                && !investPrec.getText().toString().trim().isEmpty()){
+            return true;
+
+        }else {
+            // show error message
+            new SweetAlertDialog(this, SweetAlertDialog.ERROR_TYPE)
+                    .setTitleText("Oops...")
+                    .setContentText("Please complete data!")
+                    .show();
+            return false;
+        }
+    }
+
+
+    public void addProject(View view) {
+        // check on required data
+        if (attempAdd()) {
+            if (Utils.isOnline(AddProjectActivity.this)) {
+
+                // make request
+                final SweetAlertDialog pDialog = new SweetAlertDialog(this, SweetAlertDialog.PROGRESS_TYPE);
+                pDialog.getProgressHelper().setBarColor(Color.parseColor("#A5DC86"));
+                pDialog.setTitleText("Loading...");
+                pDialog.setCancelable(false);
+                pDialog.show();
+
+                // post data
+                // Tag used to cancel the request
+                final int id = new SharknyPrefStore(this).getIntPreferenceValue(Constants.PREFERENCE_USER_AUTHENTICATION_STATE);
+                String tag_string_req = "string_req";
+                String url = BuildConfig.CreateProject;
+                // begin of request
+                VolleyMultipartRequest multipartRequest = new VolleyMultipartRequest(Request.Method.POST, url, new Response.Listener<NetworkResponse>() {
+                    @Override
+                    public void onResponse(NetworkResponse response) {
+                        pDialog.dismissWithAnimation();
+                        String resultResponse = new String(response.data);
+                        try {
+                            JSONObject result = new JSONObject(resultResponse);
+                            /*String status = result.getString("status");
+                            String message = result.getString("message");*/
+
+                            Log.d("response", resultResponse);
+                            parseFeed2(resultResponse);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        pDialog.dismissWithAnimation();
+
+                        // show error message
+                        new SweetAlertDialog(AddProjectActivity.this, SweetAlertDialog.ERROR_TYPE)
+                                .setTitleText("Oops...")
+                                .setContentText("some thing went wrong try again!")
+                                .show();
+
+                        NetworkResponse networkResponse = error.networkResponse;
+                        String errorMessage = "Unknown error";
+                        if (networkResponse == null) {
+                            if (error.getClass().equals(TimeoutError.class)) {
+                                errorMessage = "Request timeout";
+                            } else if (error.getClass().equals(NoConnectionError.class)) {
+                                errorMessage = "Failed to connect server";
+                            }
+                        } else {
+                            String result = new String(networkResponse.data);
+                            try {
+                                JSONObject response = new JSONObject(result);
+                                String status = response.getString("status");
+                                String message = response.getString("message");
+
+                                Log.e("Error Status", status);
+                                Log.e("Error Message", message);
+
+                                if (networkResponse.statusCode == 404) {
+                                    errorMessage = "Resource not found";
+                                } else if (networkResponse.statusCode == 401) {
+                                    errorMessage = message + " Please login again";
+                                } else if (networkResponse.statusCode == 400) {
+                                    errorMessage = message + " Check your inputs";
+                                } else if (networkResponse.statusCode == 500) {
+                                    errorMessage = message + " Something is getting wrong";
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        Log.i("Error", errorMessage);
+                        error.printStackTrace();
+                    }
+                }) {
+                    @Override
+                    protected Map<String, String> getParams() {
+                        Map<String, String> params = new HashMap<>();
+//                        params.put("username", userName.getText().toString());
+                        params.put("title", title.getText().toString());
+                        params.put("description", describtion.getText().toString());
+                        params.put("start_date", startDate);
+                        params.put("end_date", endDate);
+                        params.put("guarantees", grantue.getText().toString());
+                        params.put("project_type", String.valueOf(returnIndex(spinner3, type)));
+                        params.put("project_field", String.valueOf(returnIndex(spinner4, field)));
+                        params.put("investment_value", investValue.getText().toString());
+                        params.put("country", String.valueOf(returnIndex(spinner5, country)));
+                        params.put("investment_percentage", investPrec.getText().toString());
+                        params.put("created_by", String.valueOf(id));
+                        return params;
+                    }
+
+                    @Override
+                    protected Map<String, DataPart> getByteData() {
+                        Map<String, DataPart> params = new HashMap<>();
+                        // file name could found file base or direct access from real path
+                        // for now just get bitmap data from ImageView
+                        if (profileImagePath != null) {
+                            params.put("image", new DataPart("file_avatar.jpg", AppHelper.getFileDataFromDrawable(AddProjectActivity.this, profileImagePath), "image/jpeg"));
+                        } else {
+                            circleImageView.buildDrawingCache();
+                            Bitmap bmap = circleImageView.getDrawingCache();
+                            params.put("image", new DataPart("file_avatar.jpg", AppHelper.getFileDataFromImage(AddProjectActivity.this, bmap), "image/jpeg"));
+                        }
+                        return params;
+                    }
+                };
+
+                AppController.getInstance().addToRequestQueue(multipartRequest);
+                // last of request
+
+
+
+            }else {
+                // show error message
+                new SweetAlertDialog(this, SweetAlertDialog.ERROR_TYPE)
+                        .setTitleText("Oops...")
+                        .setContentText("Please check your Network connection!")
+                        .show();
+            }
+        }
+    }
+
+    private int returnIndex(Spinner spinner, String value){
+        for (int i = 1; i < spinner.getCount(); i++) {
+            if (((String) spinner.getItemAtPosition(i)).equalsIgnoreCase(value))
+                return i;
+        }
+        return 0;
+    }
+
+    private void parseFeed2(String strJson) {
+
+        JSONObject jsonRootObject = null;
+        try {
+            jsonRootObject = new JSONObject(strJson);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        if (jsonRootObject.optBoolean("success") == true ) {
+            // show success dialog and go to home
+            new SweetAlertDialog(this, SweetAlertDialog.SUCCESS_TYPE)
+                    .setTitleText("Good job!")
+                    .setContentText("Your profile successfully update!")
+                    .setConfirmText("Done!")
+                    .setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                        @Override
+                        public void onClick(SweetAlertDialog sDialog) {
+                            sDialog.dismissWithAnimation();
+                            startActivity(new Intent(AddProjectActivity.this, HomeActivity.class)
+                                    .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK));
+                            overridePendingTransition(R.anim.push_up_enter, R.anim.push_up_exit);
+                        }
+                    })
+                    .show();
+
+        } else {
+            // show error message
+            new SweetAlertDialog(this, SweetAlertDialog.ERROR_TYPE)
+                    .setTitleText("Oops...")
+                    .setContentText("some thing went wrong try again!")
+                    .show();
+        }
     }
 }
