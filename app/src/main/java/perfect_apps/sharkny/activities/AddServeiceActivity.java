8e2922f9c99c1a185c5dc9375770ceeecb386b8d
Mostafa Anter <ga30000@gmail.com>
2016-06-09 01:07:20 +0200

    stable version

--- app/src/main/java/perfect_apps/sharkny/activities/AddServeiceActivity.java
+++ app/src/main/java/perfect_apps/sharkny/activities/AddServeiceActivity.java
@@ -1,6 +1,8 @@
 package perfect_apps.sharkny.activities;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
@@ -8,22 +10,44 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.akexorcist.localizationactivity.LocalizationActivity;
+import com.android.volley.NetworkResponse;
+import com.android.volley.NoConnectionError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.TimeoutError;
+import com.android.volley.VolleyError;
 import com.bumptech.glide.Glide;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import cn.pedant.SweetAlert.SweetAlertDialog;
 import me.iwf.photopicker.PhotoPickerActivity;
 import me.iwf.photopicker.utils.PhotoPickerIntent;
+import perfect_apps.sharkny.BuildConfig;
 import perfect_apps.sharkny.R;
+import perfect_apps.sharkny.app.AppController;
+import perfect_apps.sharkny.store.SharknyPrefStore;
+import perfect_apps.sharkny.utils.AppHelper;
+import perfect_apps.sharkny.utils.Constants;
+import perfect_apps.sharkny.utils.Utils;
+import perfect_apps.sharkny.utils.VolleyMultipartRequest;
 
 public class AddServeiceActivity extends LocalizationActivity {
     @Bind(R.id.select_profile_pic)
@@ -31,6 +55,11 @@ public class AddServeiceActivity extends LocalizationActivity {
     @Bind(R.id.user_avatar)
     ImageView circleImageView;
     private static final int REQUEST_CODE = 1;
+
+    // editText
+    @Bind(R.id.editText1) EditText title;
+    @Bind(R.id.editText2) EditText describtion;
+    private static Uri profileImagePath;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -112,6 +141,7 @@ public class AddServeiceActivity extends LocalizationActivity {
                 ArrayList<String> photos =
                         data.getStringArrayListExtra(PhotoPickerActivity.KEY_SELECTED_PHOTOS);
                 Uri uri = Uri.fromFile(new File(photos.get(0)));
+                profileImagePath = uri;
                 setSelectedPhotoInsideCircleShap(uri);
             }
         }
@@ -125,6 +155,176 @@ public class AddServeiceActivity extends LocalizationActivity {
                 .placeholder(R.drawable.__picker_ic_photo_black_48dp)
                 .error(R.drawable.__picker_ic_broken_image_black_48dp)
                 .into(circleImageView);
+    }
+
+    private boolean attempAdd(){
+        if (!title.getText().toString().trim().isEmpty()
+                && !describtion.getText().toString().trim().isEmpty()){
+            return true;
+
+        }else {
+            // show error message
+            new SweetAlertDialog(this, SweetAlertDialog.ERROR_TYPE)
+                    .setTitleText("Oops...")
+                    .setContentText("Please complete data!")
+                    .show();
+            return false;
+        }
+    }
+
+    public void addService(View view) {
+        // check on required data
+        if (attempAdd()) {
+            if (Utils.isOnline(AddServeiceActivity.this)) {
+
+                // make request
+                final SweetAlertDialog pDialog = new SweetAlertDialog(this, SweetAlertDialog.PROGRESS_TYPE);
+                pDialog.getProgressHelper().setBarColor(Color.parseColor("#A5DC86"));
+                pDialog.setTitleText("Loading...");
+                pDialog.setCancelable(false);
+                pDialog.show();
+
+                // post data
+                // Tag used to cancel the request
+                final int id = new SharknyPrefStore(this).getIntPreferenceValue(Constants.PREFERENCE_USER_AUTHENTICATION_STATE);
+                String tag_string_req = "string_req";
+                String url = BuildConfig.CreateService;
+                // begin of request
+                VolleyMultipartRequest multipartRequest = new VolleyMultipartRequest(Request.Method.POST, url, new Response.Listener<NetworkResponse>() {
+                    @Override
+                    public void onResponse(NetworkResponse response) {
+                        pDialog.dismissWithAnimation();
+                        String resultResponse = new String(response.data);
+                        try {
+                            JSONObject result = new JSONObject(resultResponse);
+                            /*String status = result.getString("status");
+                            String message = result.getString("message");*/
+
+                            Log.d("response", resultResponse);
+                            parseFeed2(resultResponse);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        pDialog.dismissWithAnimation();
+
+                        // show error message
+                        new SweetAlertDialog(AddServeiceActivity.this, SweetAlertDialog.ERROR_TYPE)
+                                .setTitleText("Oops...")
+                                .setContentText("some thing went wrong try again!")
+                                .show();
+
+                        NetworkResponse networkResponse = error.networkResponse;
+                        String errorMessage = "Unknown error";
+                        if (networkResponse == null) {
+                            if (error.getClass().equals(TimeoutError.class)) {
+                                errorMessage = "Request timeout";
+                            } else if (error.getClass().equals(NoConnectionError.class)) {
+                                errorMessage = "Failed to connect server";
+                            }
+                        } else {
+                            String result = new String(networkResponse.data);
+                            try {
+                                JSONObject response = new JSONObject(result);
+                                String status = response.getString("status");
+                                String message = response.getString("message");
+
+                                Log.e("Error Status", status);
+                                Log.e("Error Message", message);
+
+                                if (networkResponse.statusCode == 404) {
+                                    errorMessage = "Resource not found";
+                                } else if (networkResponse.statusCode == 401) {
+                                    errorMessage = message + " Please login again";
+                                } else if (networkResponse.statusCode == 400) {
+                                    errorMessage = message + " Check your inputs";
+                                } else if (networkResponse.statusCode == 500) {
+                                    errorMessage = message + " Something is getting wrong";
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        Log.i("Error", errorMessage);
+                        error.printStackTrace();
+                    }
+                }) {
+                    @Override
+                    protected Map<String, String> getParams() {
+                        Map<String, String> params = new HashMap<>();
+//                        params.put("username", userName.getText().toString());
+                        params.put("title", title.getText().toString());
+                        params.put("description", describtion.getText().toString());
+                        params.put("created_by", String.valueOf(id));
+                        return params;
+                    }
+
+                    @Override
+                    protected Map<String, DataPart> getByteData() {
+                        Map<String, DataPart> params = new HashMap<>();
+                        // file name could found file base or direct access from real path
+                        // for now just get bitmap data from ImageView
+                        if (profileImagePath != null) {
+                            params.put("image", new DataPart("file_avatar.jpg", AppHelper.getFileDataFromDrawable(AddServeiceActivity.this, profileImagePath), "image/jpeg"));
+                        } else {
+                            circleImageView.buildDrawingCache();
+                            Bitmap bmap = circleImageView.getDrawingCache();
+                            params.put("image", new DataPart("file_avatar.jpg", AppHelper.getFileDataFromImage(AddServeiceActivity.this, bmap), "image/jpeg"));
+                        }
+                        return params;
+                    }
+                };
+
+                AppController.getInstance().addToRequestQueue(multipartRequest);
+                // last of request
+
+
+
+            }else {
+                // show error message
+                new SweetAlertDialog(this, SweetAlertDialog.ERROR_TYPE)
+                        .setTitleText("Oops...")
+                        .setContentText("Please check your Network connection!")
+                        .show();
+            }
+        }
+    }
+
+    private void parseFeed2(String strJson) {
+
+        JSONObject jsonRootObject = null;
+        try {
+            jsonRootObject = new JSONObject(strJson);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        if (jsonRootObject.optBoolean("success") == true ) {
+            // show success dialog and go to home
+            new SweetAlertDialog(this, SweetAlertDialog.SUCCESS_TYPE)
+                    .setTitleText("Good job!")
+                    .setContentText("Your profile successfully update!")
+                    .setConfirmText("Done!")
+                    .setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                        @Override
+                        public void onClick(SweetAlertDialog sDialog) {
+                            sDialog.dismissWithAnimation();
+                            startActivity(new Intent(AddServeiceActivity.this, HomeActivity.class)
+                                    .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK));
+                            overridePendingTransition(R.anim.push_up_enter, R.anim.push_up_exit);
+                        }
+                    })
+                    .show();
+
+        } else {
+            // show error message
+            new SweetAlertDialog(this, SweetAlertDialog.ERROR_TYPE)
+                    .setTitleText("Oops...")
+                    .setContentText("some thing went wrong try again!")
+                    .show();
+        }
     }
 
 
