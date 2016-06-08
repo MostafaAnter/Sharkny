06d82b8c710dddea3a27cd7ef779bf0d70c35b74
Mostafa Anter <ga30000@gmail.com>
2016-06-08 22:02:22 +0200

    stable version

--- app/src/main/java/perfect_apps/sharkny/fragments/FragmentFout.java
+++ app/src/main/java/perfect_apps/sharkny/fragments/FragmentFout.java
@@ -2,6 +2,7 @@ package perfect_apps.sharkny.fragments;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
@@ -13,13 +14,29 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 
+import com.android.volley.Cache;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 
+import cn.pedant.SweetAlert.SweetAlertDialog;
+import perfect_apps.sharkny.BuildConfig;
 import perfect_apps.sharkny.R;
+import perfect_apps.sharkny.activities.DetailActivity;
 import perfect_apps.sharkny.activities.SearchActivity;
+import perfect_apps.sharkny.adapters.CustomArrayAdapter;
+import perfect_apps.sharkny.app.AppController;
 import perfect_apps.sharkny.models.BubleItem;
+import perfect_apps.sharkny.parse.JsonParser;
+import perfect_apps.sharkny.utils.HorizontalListView;
+import perfect_apps.sharkny.utils.Utils;
 
 /**
  * Created by mostafa on 23/05/16.
@@ -28,6 +45,8 @@ public class FragmentFout extends Fragment {
 
     // for recycler view
     private List<BubleItem> mDataset;
+    private HorizontalListView mHlvCustomList;
+    private CustomArrayAdapter adapter;
 
     // for swipe to refresh
     private SwipeRefreshLayout mSwipeRefreshLayout;
@@ -50,7 +69,23 @@ public class FragmentFout extends Fragment {
         View view = inflater.inflate(R.layout.fragment_four, container, false);
 
         // set added recycler view
+        // set added recycler view
+        mHlvCustomList = (HorizontalListView) view.findViewById(R.id.hlvCustomList);
+        mHlvCustomList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getActivity(), DetailActivity.class);
+                Bundle arguments = new Bundle();
+                arguments.putParcelable(DetailActivity.ARG_ITEM_ID, (Parcelable) mHlvCustomList.getItemAtPosition(position));
+                intent.putExtras(arguments);
+                getActivity().startActivity(intent);
+                (getActivity()).overridePendingTransition(R.anim.push_right_enter, R.anim.push_right_exit);
 
+            }
+        });
+        // adapter
+        adapter = new CustomArrayAdapter(getActivity(), mDataset);
+        mHlvCustomList.setAdapter(adapter);
         // Retrieve the SwipeRefreshLayout and ListView instances
         mSwipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.swiperefresh);
         // Set the color scheme of the SwipeRefreshLayout by providing 4 color resource ids
@@ -134,8 +169,65 @@ public class FragmentFout extends Fragment {
 
 
     private void makeNewsRequest(){
+        if(Utils.isOnline(getActivity())){
+            fetchData();
+        }else {
+            // show error message
+            new SweetAlertDialog(getActivity(), SweetAlertDialog.ERROR_TYPE)
+                    .setTitleText("Oops...")
+                    .setContentText("Please check your Network connection!")
+                    .show();
+            fetchData();
+        }
 
 
-        onRefreshComplete();
+    }
+
+    private void fetchData(){
+        if (!mSwipeRefreshLayout.isRefreshing()) {
+            mSwipeRefreshLayout.setRefreshing(true);
+        }
+
+        String url = BuildConfig.Get_Other_service_List;
+
+        Cache cache = AppController.getInstance().getRequestQueue().getCache();
+        Cache.Entry entry = cache.get(url);
+        if(entry != null){
+            try {
+                String data = new String(entry.data, "UTF-8");
+                // handle data, like converting it to xml, json, bitmap etc.,
+                mDataset.clear();
+                mDataset.addAll(0, JsonParser.parseServiceList(data));
+                adapter.notifyDataSetChanged();
+                onRefreshComplete();
+
+
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        } else{
+            // Cached response doesn't exists. Make network call here
+            StringRequest strReq = new StringRequest(Request.Method.GET,
+                    url, new Response.Listener<String>() {
+
+                @Override
+                public void onResponse(String response) {
+                    mDataset.clear();
+                    mDataset.addAll(0, JsonParser.parseServiceList(response));
+                    adapter.notifyDataSetChanged();
+                    onRefreshComplete();
+
+                }
+            }, new Response.ErrorListener() {
+
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    onRefreshComplete();
+                }
+            });
+
+            // Adding request to request queue
+            AppController.getInstance().addToRequestQueue(strReq);
+        }
     }
 }
--- app/src/main/java/perfect_apps/sharkny/parse/JsonParser.java
+++ app/src/main/java/perfect_apps/sharkny/parse/JsonParser.java
@@ -353,4 +353,52 @@ public class JsonParser {
 
     }
 
+    public static List<BubleItem> parseServiceList(String feed){
+
+        try {
+            JSONObject  jsonRootObject = new JSONObject(feed);//done
+            JSONArray jsonNewsArray = jsonRootObject.optJSONArray("data");
+            List<BubleItem> newsList = new ArrayList<>();
+            for (int i = 0; i < jsonNewsArray.length(); i++) {
+                JSONObject jsonObject = jsonNewsArray.getJSONObject(i);
+
+                String id = jsonObject.optString("id");
+                String title = jsonObject.optString("title");
+                String description = jsonObject.optString("description");
+                String image = jsonObject.optString("image") ;
+                String likes_count = jsonObject.optString("likes_count");
+                String comments_count = jsonObject.optString("comments_count");
+                String general_type = jsonObject.optString("general_type");
+                String is_verified = jsonObject.optString("is_verified");
+                String created_by = jsonObject.optString("created_by");
+
+                JSONObject ownerObject = jsonObject.optJSONObject("createdby");
+                String idOwner = ownerObject.optString("id") ;
+                String username = ownerObject.optString("username");
+                String fullname = ownerObject.optString("fullname");
+                String job = ownerObject.optString("job");
+                String address = ownerObject.optString("address");
+                String mobile = ownerObject.optString("mobile");
+                String email = ownerObject.optString("email");
+                String imageOwner = ownerObject.optString("image");
+                String gender = ownerObject.optString("gender");
+                String nationality = ownerObject.optString("nationality");
+                String countryOwner = ownerObject.optString("countryOwner");
+
+                OwnerUser ownerUser = new OwnerUser(idOwner, username, fullname, job, address, mobile, email, imageOwner, gender, nationality, countryOwner);
+                BubleItem bubleItem = new BubleItem(id, title, description,null, null,
+                        null,null,null,
+                        null, null,null, null,
+                        image,likes_count,comments_count, general_type, is_verified, created_by,ownerUser);
+                newsList.add(bubleItem);
+            }
+            return newsList;
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+
+    }
+
 }
